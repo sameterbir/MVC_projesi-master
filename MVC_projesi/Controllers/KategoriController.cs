510bf6eb3084bca63947b3c8c58e77408c595946
ABDULSAMET ERBİR <abdulsameterbir21@istanbularel.edu.tr>
2023-01-01 21:37:59 +0300

    First Commit Samet

---
+++ MVC_projesi/Controllers/KategoriController.cs
@@ -0,0 +1,74 @@
+﻿using KutuphaneProgrami.Data;
+using KutuphaneProgrami.Data.Model;
+using KutuphaneProgrami.Data.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_projesi.Controllers
+{
+    public class KategoriController : Controller
+    {
+
+        UnitOfWork unitOfWork;
+
+        public KategoriController()
+        {
+            unitOfWork = new UnitOfWork();
+        }
+
+        public ActionResult Index()
+        {
+            var kategoriler = unitOfWork.GetRepository<Kategori>().GetAll();
+
+            return View(kategoriler);
+        }
+
+
+
+        [HttpPost]
+        public JsonResult EkleJson(string ktgAd)
+        {
+            Kategori ktgri = new Kategori();
+            ktgri.Ad = ktgAd;
+            var eklenenKtg = unitOfWork.GetRepository<Kategori>().Add(ktgri);
+            unitOfWork.SaveChanges();
+            return Json(
+                new
+                {
+                    Result = new
+                    {
+
+                        eklenenKtg.Id,
+                        eklenenKtg.Ad
+                        
+
+                    }, JsonRequestBehavior.AllowGet
+                }
+            ); 
+        }
+        [HttpPost]
+        public JsonResult GuncelleJson(int ktgId , string ktgAd)
+        {
+            var kategori = unitOfWork.GetRepository<Kategori>().GetById(ktgId);
+            kategori.Ad = ktgAd;
+            var durum = unitOfWork.SaveChanges();
+            if (durum > 0) return Json("1");
+            return Json("0");
+        }
+
+
+        [HttpPost]
+        public JsonResult SilJson(int ktgId)
+        {
+            unitOfWork.GetRepository<Kategori>().Delete(ktgId);     
+            var durum = unitOfWork.SaveChanges();
+            if (durum > 0) return Json("1");
+            return Json("0");
+        }
+
+
+    }
+}
---
+++ MVC_projesi/Controllers/OduncKitapController.cs
@@ -0,0 +1,89 @@
+﻿using KutuphaneProgrami.Data.Model;
+using KutuphaneProgrami.Data.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_projesi.Controllers
+{
+    public class OduncKitapController : Controller
+    {
+
+        UnitOfWork unitOfWork;
+
+        public OduncKitapController()
+        {
+            unitOfWork = new UnitOfWork();
+        
+        }
+
+        public ActionResult VerilenKitap() 
+        {
+            var oduncKitap = unitOfWork.GetRepository<OduncKitap>().GetAll();
+            return View(oduncKitap);
+
+        }
+
+        public ActionResult TeslimEdilenKitap()
+        {
+
+            return View();
+
+        }
+
+        public ActionResult KitapVer()
+        {
+            ViewBag.Kitaplar = unitOfWork.GetRepository<Kitap>().GetAll(x => x.Adet > 0);
+            ViewBag.Uyeler = unitOfWork.GetRepository<Uye>().GetAll();
+            return View();
+
+        }
+
+        [HttpPost]
+        public JsonResult KitapVerJson(int uyeId , int kitapId , DateTime getirecegiTarih)
+        {
+            OduncKitap oduncKitap = new OduncKitap();
+            oduncKitap.AlisTarihi = DateTime.Now;
+            oduncKitap.GetirecegiTarih = getirecegiTarih;
+            oduncKitap.KitapId = kitapId;
+            oduncKitap.UyeId = uyeId;
+            unitOfWork.GetRepository<OduncKitap>().Add(oduncKitap);
+            var durum = unitOfWork.SaveChanges();
+            if (durum > 0)
+                return Json("1");
+            else
+                return Json("0");
+
+        }
+
+        public ActionResult VerilenKitabiGuncelle(int oduncKitapId)
+        {
+            ViewBag.Kitaplar = unitOfWork.GetRepository<Kitap>().GetAll(x => x.Adet > 0);
+            ViewBag.Uyeler = unitOfWork.GetRepository<Uye>().GetAll();
+            var oduncKitap = unitOfWork.GetRepository<OduncKitap>().GetById(oduncKitapId);
+            return View(oduncKitap);
+        
+        }
+
+
+        [HttpPost]
+        public JsonResult VerilenKitabiGuncelleJson(int oduncKitapId, int uyeId, int kitapId, DateTime getirecegiTarih)
+        {
+            var oduncKitap = unitOfWork.GetRepository<OduncKitap>().GetById(oduncKitapId);
+            oduncKitap.GetirecegiTarih = getirecegiTarih;
+            oduncKitap.KitapId = kitapId;
+            oduncKitap.UyeId = uyeId;
+            unitOfWork.GetRepository<OduncKitap>().Update(oduncKitap);
+            var durum = unitOfWork.SaveChanges();
+            if (durum > 0)
+                return Json("1");
+            else
+                return Json("0");
+
+        }
+
+    }
+
+}
